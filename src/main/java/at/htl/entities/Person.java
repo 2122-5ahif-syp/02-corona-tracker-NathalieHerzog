86f4f4f451165a817bd86da96714007c3e625d10
NathalieHerzog <nathalie.herzog03@gmail.com>
2021-10-21 14:04:14 +0200

    Add person entity

---
+++ src/main/java/at/htl/entities/Person.java
@@ -0,0 +1,66 @@
+package at.htl.entities;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id")
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String eMail;
+    private String phoneNumber;
+    private Instant timeStamp;
+
+    public Person(String firstName, String lastName, String eMail, String phoneNumber, Instant timeStamp) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.eMail = eMail;
+        this.phoneNumber = phoneNumber;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Instant getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Instant timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
